cdda535803c2211662bc3f01b7d69f2db551aeec
przemekr95 <przemekrura95@gmail.com>
2021-11-18 16:19:34 +0100

    Added basic button and eslint config

--- src/components/pages/test/Test.jsx
+++ src/components/pages/test/Test.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import './Test.scss';
+
+import Button from '../../common/button';
+
 function Test() {
-  return <section>Test</section>;
+  return (
+    <section className="test-wrapper">
+      <Button text="Seniorzy" appearance="primary" corner="squere" />
+    </section>
+  );
 }
 
 export default Test;
