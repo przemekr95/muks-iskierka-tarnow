091dd45794f19247785dd370b13bff1bca9b8015
przemekr95 <przemekrura95@gmail.com>
2021-11-23 22:27:59 +0100

    Created button

--- src/components/pages/test/Test.jsx
+++ src/components/pages/test/Test.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './Test.scss';
 
-import Button from '../../common/button';
-
 function Test() {
   return (
     <section className="test-wrapper">
-      <Button text="Seniorzy" appearance="primary" corner="squere" />
+      tse
     </section>
   );
 }
