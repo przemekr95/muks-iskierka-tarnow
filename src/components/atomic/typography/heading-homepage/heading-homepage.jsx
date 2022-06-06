00f6e8a96276a77f3f4fad6f4ecd0920c373e2b1
przemekr95 <przemekrura95@gmail.com>
2022-06-06 14:36:53 +0200

    Removed comments

--- src/components/atomic/typography/heading-homepage/heading-homepage.jsx
+++ src/components/atomic/typography/heading-homepage/heading-homepage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Text from '../text';
 
 import './heading-homepage.scss';
 
 const HeadingHomepage = props => {
   const {
-    // color,
-    // appearance,
-    // tag,
     text,
   } = props;
 
@@ -20,15 +16,7 @@ const HeadingHomepage = props => {
 };
 
 HeadingHomepage.propTypes = {
-  //   color: PropTypes.string,
-  //   appearance: PropTypes.string.isRequired,
-  //   tag: PropTypes.string,
   text: PropTypes.string.isRequired,
 };
-
-// HeadingHomepage.defaultProps = {
-//   color: 'black_text',
-//   tag: 'h1',
-// };
 
 export default HeadingHomepage;
