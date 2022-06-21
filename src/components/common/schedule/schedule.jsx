08f22f5d4fb1c4f5eecb18f3e13ca3d1a4c0db0b
przemekr95 <przemekrura95@gmail.com>
2022-06-21 20:55:08 +0200

    Fixed styles in third-row

--- src/components/common/schedule/schedule.jsx
+++ src/components/common/schedule/schedule.jsx
@@ -18,37 +18,61 @@ const Schedule = () => {
   const tableSchedule = (
     items.map(item => (
       <tr key={item.id} className="row team-schedule">
-        <td data-label="Date">
+        <td data-label="Date" className="schedule-date">
           <Text
             text={item.date}
+            color="light_grey_text"
+            font="regular_font"
+          />
+        </td>
+        <td data-label="Home" className="team-schedule team-schedule-box">
+          <div className="team-schedule-name text-left">
+            <Text
+              text={item.home}
+              color="black_text"
+              font="regular_font"
+            />
+            <Text
+              text={item.homeCity}
+              color="blue_text"
+              font="bold_font"
+            />
+          </div>
+          <div className="schedule-logo-box">
+            <Image
+              src={item.homeLogo}
+              alt={item.homeLogoAlt}
+              className="schedule-logo"
+            />
+          </div>
+        </td>
+        <td data-label="VS">
+          <Text
+            text="vs"
             color="black_text"
             font="regular_font"
           />
         </td>
-        <td data-label="Home" className="team-schedule">
-          <Image
-            src={item.homeLogo}
-            alt={item.homeLogoAlt}
-            className="schedule-logo"
-          />
-          <Text
-            text={item.home}
-            color="black_text"
-            font="regular_font"
-          />
-        </td>
-        <td data-label="VS">vs</td>
-        <td data-label="Away" className="team-schedule">
-          <Text
-            text={item.away}
-            color="black_text"
-            font="regular_font"
-          />
-          <Image
-            src={item.awayLogo}
-            alt={item.awayLogoAlt}
-            className="schedule-logo"
-          />
+        <td data-label="Away" className="team-schedule team-schedule-box">
+          <div className="schedule-logo-box">
+            <Image
+              src={item.awayLogo}
+              alt={item.awayLogoAlt}
+              className="schedule-logo"
+            />
+          </div>
+          <div className="team-schedule-name text-right">
+            <Text
+              text={item.away}
+              color="black_text"
+              font="regular_font"
+            />
+            <Text
+              text={item.awayCity}
+              color="blue_text"
+              font="bold_font"
+            />
+          </div>
         </td>
       </tr>
     ))
